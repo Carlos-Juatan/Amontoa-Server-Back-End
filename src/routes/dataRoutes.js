cc079a4131f604a27ad96f1e690916f0711bc1de
Carlos Juatan <carlosjuatan.workplace@gmail.com>
2024-12-11 23:48:42 -0300

    Server Back-end funcional
    - Adicionado funçoes funcionais

--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import { database } from '../controllers/dataController.js';
+
+const corsOptions = {
+    origin: 'http://localhost:8000',
+    optionsSuccessStatus: 200
+}
 
 const routes = (app) => {
     app.use(express.json());
+    app.use(cors(corsOptions));
     
     app.get('/data', database);
 }
